87f83cc0793ed2e02298b6ea9ddb0fbdb7003de0
fady nabil <fadynano@gmail.com>
2024-11-19 03:26:20 +0200

    setup project base

---
+++ src/i18n.js
@@ -0,0 +1,13 @@
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import enJSON from './locale/en.json'
+import arJSON from './locale/ar.json'
+
+
+i18n.use(initReactI18next).init({
+ resources: {
+    ar: {...arJSON},
+    en: {...enJSON}
+ }, 
+ lng: "ar",     
+});
